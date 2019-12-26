a9fb9e8e16ba4614955433dfa20336f388dcf8c8
jonathanlurie <lurie.jo@gmail.com>
2019-12-26 22:29:58 +0100

    WIP

--- dist/duskrcore.js
+++ dist/duskrcore.js
@@ -7322,10 +7322,12 @@
             };
 
             class AdobeXmp {
+
               constructor(){
                 this._xmlPayload = null;
                 this._objectRepresentation = null;
               }
+
 
               setXmlPayload(xmlStr){
                 this._xmlPayload = xmlStr;
@@ -7333,9 +7335,49 @@
                 this._objectRepresentation = JSON.parse(jsonPayload);
               }
 
+
               getXml(){
                 return lib.json2xml(JSON.stringify(this._objectRepresentation), {compact: true, ignoreComment: true, spaces: 4})
               }
+
+
+              _getBasicAttributesObject(){
+                return this._objectRepresentation['x:xmpmeta']['rdf:RDF']['rdf:Description']['_attributes']
+              }
+
+
+              hasSettings(){
+                let ba = this._getBasicAttributesObject();
+                return ba['crs:HasSettings'] === 'True'
+              }
+
+
+              getRawFileName(){
+                let ba = this._getBasicAttributesObject();
+                return ba['crs:RawFileName']
+              }
+
+
+              setRawFileName(name){
+                let ba = this._getBasicAttributesObject();
+                ba['crs:RawFileName'] = name;
+              }
+
+
+              getSettingAttribute(attrName){
+                let ba = this._getBasicAttributesObject();
+                if(`crs:${attrName}` in ba){
+                  return ba[`crs:${attrName}`]
+                }else{
+                  return null
+                }
+              }
+
+              
+              setSettingAttrubute(attrName, value){
+
+              }
+
             }
 
             var index = { AdobeXmp };
--- es/duskrcore.js
+++ es/duskrcore.js
@@ -1,10 +1,12 @@
 import XmlParser from 'xml-js';
 
 class AdobeXmp {
+
   constructor(){
     this._xmlPayload = null;
     this._objectRepresentation = null;
   }
+
 
   setXmlPayload(xmlStr){
     this._xmlPayload = xmlStr;
@@ -12,9 +14,49 @@ class AdobeXmp {
     this._objectRepresentation = JSON.parse(jsonPayload);
   }
 
+
   getXml(){
     return XmlParser.json2xml(JSON.stringify(this._objectRepresentation), {compact: true, ignoreComment: true, spaces: 4})
   }
+
+
+  _getBasicAttributesObject(){
+    return this._objectRepresentation['x:xmpmeta']['rdf:RDF']['rdf:Description']['_attributes']
+  }
+
+
+  hasSettings(){
+    let ba = this._getBasicAttributesObject();
+    return ba['crs:HasSettings'] === 'True'
+  }
+
+
+  getRawFileName(){
+    let ba = this._getBasicAttributesObject();
+    return ba['crs:RawFileName']
+  }
+
+
+  setRawFileName(name){
+    let ba = this._getBasicAttributesObject();
+    ba['crs:RawFileName'] = name;
+  }
+
+
+  getSettingAttribute(attrName){
+    let ba = this._getBasicAttributesObject();
+    if(`crs:${attrName}` in ba){
+      return ba[`crs:${attrName}`]
+    }else{
+      return null
+    }
+  }
+
+  
+  setSettingAttrubute(attrName, value){
+
+  }
+
 }
 
 var index = { AdobeXmp };
